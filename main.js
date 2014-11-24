8919470fe1d76b4612d5f144a1e7b0b873b2acc9
Patrick Burtchaell <burtchaell@users.noreply.github.com>
2014-11-24 10:47:24 -0600

    Update main.js
    - Fix spacing
    - Switch from double quotes to single quotes
    - Refactor `defineLanguage()` function

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 define(function (require, exports, module) {
+  
   "use strict";
-  var LanguageManager = brackets.getModule("language/LanguageManager");
-  LanguageManager.defineLanguage("lens", {
-      "name":"lens",
-      "mode":"lens",
-      "fileExtensions":["lens"],
-      "lineComment":["#"]
+  
+  var LanguageManager = brackets.getModule('language/LanguageManager');
+  
+  var definition = {
+    name: 'Lens',
+    mode: ['htmlmixed', 'text/x-lens-html'],
+    fileExtensions: ['lens'],
+    lineComment: ['#']
+  };
+  
+  LanguageManager.defineLanguage('lens', definition).done(function (language) {
+    console.log('Language ' + language.getName() + ' is available.');
   });
+
 });
